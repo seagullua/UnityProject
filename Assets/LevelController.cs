8c1a3509d028ed8adf359303ccd5e10f78aa2004
Kitske <alexander7dvorak@gmail.com>
2018-06-18 05:04:43 +0300

    2 Task

---
+++ Assets/LevelController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    public static LevelController current;
+    Vector3 startingPosition;
+
+    void Awake()
+    {
+        current = this;
+    }
+
+    public void setStartPosition(Vector3 pos)
+    {
+        this.startingPosition = pos;
+    }
+
+    public void onRabbitDeath(Rabbit rabbit)
+    {
+        //        При        смерті    кролика    повертаємо    на    початкову    позицію    
+        rabbit.transform.position = this.startingPosition;
+    }
+}
--- Assets/Rabbit.cs
+++ Assets/Rabbit.cs
@@ -5,23 +5,83 @@ using UnityEngine;
 public class Rabbit : MonoBehaviour
 {
     public float speed = 1;
-    float value = Input.GetAxis("Horizontal");
+    bool isGrounded;
+    bool JumpActive = false;
+    float JumpTime = 0f;
+    public float MaxJumpTime = 2f;
+    public float JumpSpeed = 2f;
     Rigidbody2D myBody = null;
     // Use this for initialization
     void Start()
     {
         myBody = this.GetComponent<Rigidbody2D>();
+        LevelController.current.setStartPosition(transform.position);
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 from = transform.position + Vector3.up * 0.3f;
+        Vector3 to = transform.position + Vector3.down * 0.1f;
+        int layer_id = 1 << LayerMask.NameToLayer("Ground");
+        //        Перевіряємо        чипроходитьлініячерезColliderзшаромGround
+        RaycastHit2D hit = Physics2D.Linecast(from, to, layer_id);
+        if (hit)
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+        //        Намалюватилінію(длярозробника)
+        Debug.DrawLine(from, to, Color.red);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            this.JumpActive = true;
+        }
+        if (this.JumpActive)
+        {            //            Якщо            кнопку        ще        тримають    
+            if (Input.GetButton("Jump"))
+            {
+                this.JumpTime += Time.deltaTime;
+                if (this.JumpTime < this.MaxJumpTime)
+                {
+                    Vector2 vel = myBody.velocity;
+                    vel.y = JumpSpeed * (1.0f - JumpTime / MaxJumpTime);
+                    myBody.velocity = vel;
+                }
+            }
+            else
+            {
+                this.JumpActive = false; this.JumpTime = 0;
+            }
+        }
+
         float value = Input.GetAxis("Horizontal");
+        Animator animator = GetComponent<Animator>();
+
+        if (this.isGrounded)
+        {
+            animator.SetBool("jump", false);
+        }
+        else
+        {
+            animator.SetBool("jump", true);
+        }
+
         if (Mathf.Abs(value) > 0)
         {
+            animator.SetBool("run", true);
             Vector2 vel = myBody.velocity;
             vel.x = value * speed; myBody.velocity = vel;
         }
+        else
+        {
+            animator.SetBool("run", false);
+        }
+
         SpriteRenderer sr = GetComponent<SpriteRenderer>();
         if (value < 0)
         {
@@ -31,6 +91,7 @@ public class Rabbit : MonoBehaviour
         {
             sr.flipX = false;
         }
+
 
     }
 
